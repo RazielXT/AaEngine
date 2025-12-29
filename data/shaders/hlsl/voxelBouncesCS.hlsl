57280ceae97f9f7086b5698772621d60ebc23c12
marek tomco <marektomco@gmail.com>
2025-12-29 13:59:46 +0100

    wave optimize voxel bounces

--- data/shaders/hlsl/voxelBouncesCS.hlsl
+++ data/shaders/hlsl/voxelBouncesCS.hlsl
@@ -15,17 +15,25 @@ SamplerState VoxelSampler : register(s0);
 void main(uint3 id : SV_DispatchThreadID)
 {
 	uint linearIndex = id.z * (128 * 128) + id.y * 128 + id.x;
-	float3 worldNormal = VoxelData[linearIndex].Normal;
 
-	float3 voxelUV = (float3(id) + 0.5f) / 128;
-	float4 fullTraceSample = ConeTrace(voxelUV, worldNormal, 1.05f, 1.5f, SceneVoxelBounces, VoxelSampler);
+	float occupied = VoxelData[linearIndex].Occupy;
+
+	if (WaveActiveAllTrue(occupied == 0))
+		return;
+	if (occupied == 0)
+		return;
 
 	float4 baseColor = VoxelData[linearIndex].Diffuse;
+	float3 worldNormal = VoxelData[linearIndex].Normal;
+	float3 voxelUV = (float3(id) + 0.5f) / 128;
+
+	float4 fullTraceSample = ConeTrace(voxelUV, worldNormal, 1.05f, 1.5f, SceneVoxelBounces, VoxelSampler);
+
 	float3 bounceColor = baseColor.rgb * fullTraceSample.rgb * 0.5f;
 	float shadow = VoxelData[linearIndex].Max / 100.f;
 	bounceColor += baseColor.rgb * shadow * 0.5f;
 
-	float3 prevBounceColor = SceneVoxelBounces[id].rgb;
+	//float3 prevBounceColor = SceneVoxelBounces[id].rgb;
 	float3 currentLightBounce = bounceColor;
 
 	float3 camOffset = (CameraPosition - VoxelsOffset) / VoxelsSceneSize;
@@ -33,5 +41,5 @@ void main(uint3 id : SV_DispatchThreadID)
 	float3 distanceFade = saturate(-4 * (diffOffset - 0.5) + 1);
 	float fade = min(min(distanceFade.x, distanceFade.y), distanceFade.z);
 
-    VoxelTexture[id] = float4(currentLightBounce, VoxelData[linearIndex].Occupy) * saturate(fade);
+	VoxelTexture[id] = float4(currentLightBounce, occupied) * saturate(fade);
 }

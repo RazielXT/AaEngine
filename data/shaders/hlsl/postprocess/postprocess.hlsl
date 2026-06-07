f9d9360d2d5295be1be7a2b38437b90b72f85b81
marek tomco <marektomco@gmail.com>
2026-06-07 21:25:44 +0200

    post process materials definition refactor

--- data/shaders/hlsl/postprocess/postprocess.hlsl
+++ data/shaders/hlsl/postprocess/postprocess.hlsl
@@ -2,19 +2,6 @@
 
 float2 ViewportSizeInverse;
 
-// Vertex shader: self-created quad.
-VS_OUTPUT VSQuad(uint vI : SV_VertexId)
-{
-    VS_OUTPUT vout;
-
-    // We use the 'big triangle' optimization so you only Draw 3 verticies instead of 4.
-    float2 texcoord = float2((vI << 1) & 2, vI & 2);
-    vout.TexCoord = texcoord;
-
-    vout.Position = float4(texcoord.x * 2 - 1, -texcoord.y * 2 + 1, 0, 1);
-    return vout;
-}
-
 Texture2D colorMap : register(t0);
 Texture2D colorMap2 : register(t1);
 SamplerState LinearSampler : register(s0);
